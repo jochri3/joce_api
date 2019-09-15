534481ba21b289febfbf7ac18dffca3231f848ec
Christian Lisangola <christian.lisangola@gmail.com>
2019-09-15 22:01:52 +0100

    Added cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 require("./services/db_init");
 
